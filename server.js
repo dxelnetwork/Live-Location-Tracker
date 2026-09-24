6d8fe0f63dc9e48bfa03a76242401110b3e8720c
DXEL Network <dxel@outlook.com>
2026-09-24 18:45:59 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const cookieParser = require("cookie-parser")
+const socketIO = require("socket.io")
+const config = require("./config")
+const express = require("express")
+const tarkine = require("tarkine")
+const http = require("http")
+
+const app = express()
+const server = http.createServer(app)
+const io = new socketIO.Server(server)
+
+const PORT = process.env.PORT || config.port
+
+global.IO = io
+
+app.set("view engine", "html")
+app.engine("html", tarkine.renderFile)
+
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(__dirname + "/public"))
+app.use(express.json())
+
+app.use("/", require("./router"))
+
+server.listen(PORT, () => {
+    console.log(`Live Location Tracker running on port ${PORT}`)
+})
